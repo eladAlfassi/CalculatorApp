40afa4f2e679b417e6c0596ae65304404fb2001c
isaac <isaac21e@gmail.com>
2018-05-10 16:07:07 -0700

    do_post server code that worked with docker-compose

--- server.py
+++ server.py
@@ -1,42 +1,60 @@
 # !/usr/bin/env python
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from io import BytesIO
 import json
-import HttpResponseDict
-from request_to_executor_mapper import *
+import os
 
 
 # HTTPRequestHandler class
-class CalculatorServer(BaseHTTPRequestHandler):
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        request_body = self.rfile.read(content_length)
-        args = json.load(request_body)
-        executor = RequestToExecutorMapper.get_executor(self.path)
-        response = executor.execute(args)
-        self.respond(self,response)
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
 
-    def respond(self, response):
-        httpResponseDict = HttpResponseDict()
-        self.send_response(httpResponseDict[response['header']])
+        # Send headers
         self.send_header('Content-type', 'text/json')
         self.end_headers()
-        message_as_json = json.dumps(response['body'])
-        # # Write content as utf-8 data
+
+        # Send message back to client
+        message = {"display": "55"}
+        message_as_json = json.dumps(message)
+        # Write content as utf-8 data
         self.wfile.write(bytes(message_as_json, "utf8"))
+        return
+
+    def do_POST(self):
+        print("SERVER: in do post")
+        content_length = int(self.headers['Content-Length'])
+        message = {"display": "55"}
+        message_as_json = json.dumps(message)
+        got_from_client = self.rfile.read(content_length).decode('utf-8')
+        print("SERVER: got from client: "+ got_from_client )
+        body = message_as_json
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(bytes(body, "utf8"))
+        print("SERVER: returning: "+body)
+        self.wfile.write(response.getvalue())
 
 
 def run():
-    config = json.load(open('config.json'))
-    port = config['port']
+    import os
+    #config = json.load(open('config.json'))
+    #port=config['port']
     print('starting server...')
 
     # Server settings
     # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('127.0.0.1', port)
-    httpd = HTTPServer(server_address, CalculatorServer)
+    port=5000
+    server_address = ('0.0.0.0', port)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
     print('running server...')
+    print('running on port ...'+str(port))
+
     httpd.serve_forever()
 
 
